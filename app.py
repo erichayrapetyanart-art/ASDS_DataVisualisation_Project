581c8f3d2c3b9e9b40c620c1fb10e5ee205cf1c6
Erik Hayrapetyan <erikhayrapetyan@Eriks-MacBook-Pro.local>
2026-05-23 13:48:27 +0400

    Update app.py configuration

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ server = app.server
 # ============================================
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8050)
+    app.run(host="0.0.0.0", port=8050, debug=False)
